995e596c1d595a68dff3531611aab7ecfbc93d6b
Enrique Garcia Rascon <enriquegr@Enriques-iMac.local>
2024-02-23 05:11:07 -0600

    Descripción de los cambios realizados

--- test_pdftotext.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-try:
-    # Intenta ejecutar 'pdftotext' y obtener la versión
-    result = subprocess.run(["pdftotext", "-v"], capture_output=True, text=True, check=True)
-    print("pdftotext output:", result.stdout)
-    print("pdftotext error output:", result.stderr)  # Imprime la salida de error estándar
-except subprocess.CalledProcessError as e:
-    print("Error al ejecutar pdftotext:", e)
